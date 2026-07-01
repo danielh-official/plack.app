f87dc043a85a508ef721c3a8217574939b6bdc91
nuno maduro <enunomaduro@gmail.com>
2026-07-01 15:15:29 +0100

    feat(workspaces): actions

---
+++ tests/Unit/Actions/CreateWorkspaceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Actions\CreateWorkspace;
+use App\Models\User;
+use App\Models\Workspace;
+
+it('may create workspaces', function (): void {
+    $user = User::factory()->create();
+
+    $workspace = resolve(CreateWorkspace::class)->handle(
+        $user,
+        'subscribe the channel',
+    );
+
+    expect($workspace)
+        ->toBeInstanceOf(Workspace::class)
+        ->and($workspace->owner->id)->toBe($user->id)
+        ->and($workspace->name)->toBe('subscribe the channel');
+});
